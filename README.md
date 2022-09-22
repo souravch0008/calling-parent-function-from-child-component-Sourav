cbb9ecae5327e573378db86ce53d69386ef1fe11
souravch0008 <114172854+souravch0008@users.noreply.github.com>
2022-09-22 21:12:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# calling-parent-function-from-child-component-cym4v4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/calling-parent-function-from-child-component-cym4v4)
---
+++ src/app/child-component/child-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child-component',
+  templateUrl: './child-component.component.html',
+  styleUrls: ['./child-component.component.css'],
+})
+export class ChildComponentComponent implements OnInit {
+  @Input() name;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
